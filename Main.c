1d8cbef44593afcfb02c6a1b6ee3cdb37d918cfa
matheus <mdqueiroz12@gmail.com>
2021-04-05 01:19:12 -0300

    Commit inicial

---
+++ Main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "cartas.c"
+
+int main(){
+    B1 *ptrB1;
+    ptrB1 = (B1*) malloc(sizeof(B1));
+}
---
+++ cartas.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct sB1{
+	char carta[2];
+}B1;
+
+
+void inicializaBaralho(){
+	
+}
+
