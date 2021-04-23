b1436f8665b9639f2e692b410d3459ccc49bce21
Matheus D Queiroz <32309799+mdqueiroz1@users.noreply.github.com>
2021-04-23 14:04:16 -0300

    adição da função de inicializar o baralho

--- Baralho.c
+++ Baralho.c
@@ -19,15 +19,69 @@ typedef struct sBaralhoPadrao{
 	struct sBaralho *prox;
 }baralhoPadrao;
 
-typedef struct sB1{
+typedef struct sBaralho{
 	char carta[2];
-}B1;
+}Baralho;
+
 /*
 Função feita para criar o baralho completo de A a K com todos
 os naipes em ordem para depois serem embaralhados. 
 */
 void inicializaBaralho(baralhoPadrao **ptrBaralho){
-//itoa(inteiro, string, 10 - decimal);
+    char str[3];
+    char naipe[1];
+    for(int i = 0; i < 4; i++){
+        switch (i){
+            case 0:
+                strcpy(naipe,"O");
+                break;
+            
+            case 1:{
+                strcpy(naipe , "P");
+                break;
+            }
+            
+            case 2:{
+                strcpy(naipe , "E");
+                break;
+            }
+            
+            case 3:{
+                strcpy(naipe , "C");
+                break;
+            }
+        }
+        for (int j = 0; j < 13; j++){
+            int num = j+1;
+            if(num == 1 || num > 10){
+                switch (num){
+                    case 1:{
+                        strcpy(str, "A");
+                        strncat(str, naipe,1);
+                        break;
+                    }
+                    case 11:{
+                        strcpy(str, "J");
+                        strncat(str, naipe,1);                        
+                        break;
+                    }
+                    case 12:{
+                        strcpy(str, "Q");
+                        strncat(str, naipe,1);
+                        break;
+                    }
+                    case 13:{
+                        strcpy(str, "K");
+                        strncat(str, naipe,1);
+                    }
+                }
+            }else{
+                strncat(itoa(num, str, 10), naipe,1);
+            }
+            
+            inserirInicio(*ptrBaralho,str);
+        }
+    }
 }
 
 /*
@@ -35,6 +89,41 @@ Função feita para construir o baralho embaralhado, pegando
 o padrão, buscando um numero entre 1 e 54 e alocando essa carta
 no deque de cartas embaralhadas.
 */
-void shuffle(baralhoPadrao **ptrBaralho, ){
+void shuffle(baralhoPadrao **ptrBaralho ){
 
+}
+
+void inserirInicio(baralhoPadrao **ptrLista, char *elem){
+    baralhoPadrao *novo;
+    novo = alocarNo();
+    if(novo != NULL){
+        strcpy(novo->carta, elem);
+		
+		if(*ptrLista == NULL){
+			*ptrLista = novo;
+			novo->prox = NULL;
+		
+		}else{
+        	novo->prox = *ptrLista;
+			*ptrLista = novo;
+		}
+    }else{
+        printf("Erro ao alocar o no.\n");
+        exit(1);
+    }
+}
+
+int imprimeLista(baralhoPadrao *ptrLista){
+    baralhoPadrao *aux;
+    if(listaVazia(ptrLista)){
+        printf("lista vazia.\n");
+        return 1;    
+    }else{
+        aux = ptrLista;
+        while(aux != NULL){
+            printf("%s\t",ptrLista->carta);
+            aux = aux->prox;  
+        }
+    }
+    return 0;
 }
