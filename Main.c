1d2c4ecd23103e74595a63c1df7d802783a387dd
Matheus D Queiroz <32309799+mdqueiroz1@users.noreply.github.com>
2021-05-08 09:33:23 -0300

    Adição da arte ASCII do menu

--- Main.c
+++ Main.c
@@ -10,8 +10,20 @@
 
 int menu(Fila *ptrF){
     int loop;
-    printf("1- Iniciar jogo\n");
-    printf("2- Creditos\n");
+    system("cls");
+	printf("8888888b.  d8b 8888888888       8888888b.\n");           
+	printf("888   Y88b Y8P 888              888  \"Y88b\n");          
+	printf("888    888     888              888    888\n");          
+	printf("888   d88P 888 8888888  8888b.  888    888  .d88b.\n");  
+	printf("8888888P\"  888 888         \"88b 888    888 d88\"\"88b\n"); 
+	printf("888        888 888     .d888888 888    888 888  888\n"); 
+	printf("888        888 888     888  888 888  .d88P Y88..88P\n"); 
+	printf("888        888 888     \"Y888888 8888888P\"   \"Y88P\"\n");
+	printf("\t\tPife Adaptado\n\n");         
+
+ 	printf("1 - Iniciar Jogo\n");
+    printf("2 - Creditos\n");
+    printf("9 - Sair do programa\n");
     scanf("%d", &loop);
     switch (loop){
         case 1:{
@@ -19,7 +31,12 @@ int menu(Fila *ptrF){
             break;
         }
         case 2:{
-            printf("Matheus Dias de Queiroz\n Marcos Aurelio Duarte Souza\n");
+            printf("  _____________________________\n");
+            printf(" |       Pife Adaptado         |\n");
+            printf(".-------------------------------.\n");
+            printf("|    Matheus Dias de Queiroz    |\n");
+            printf("|  Marcos Aurelio Duarte Souza  |\n");
+            printf("'-------------------------------'\n");
             break;
         }
    
@@ -42,7 +59,7 @@ int main(){
 
     do{
         loop = menu(&Baralho1);
-    }while(loop != 1);
+    }while(loop != 9);
     
     return 0;
 }
